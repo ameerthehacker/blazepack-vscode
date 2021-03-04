9e34168d4cd8ea93b3c09e605c9a42179f052f55
Ameer Jhan <ameerjhanprof@gmail.com>
2021-03-04 23:19:55 +0530

    feat: naive implementation of blazepack dev server

--- extension.js
+++ extension.js
@@ -1,19 +1,21 @@
 const vscode = require('vscode');
+const bp = require('blazepack');
 
 function activate(context) {
+	let disposable = vscode.commands.registerCommand('blazepack.startDevServer', async () => {
+		const workspaceFolders = vscode.workspace.workspaceFolders;
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "blazepack" is now active!');
+		if (workspaceFolders) {
+			const directory = workspaceFolders[0].uri.path;
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with  registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('blazepack.helloWorld', function () {
-		// The code you place here will be executed every time your command is executed
-
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from blazepack!');
+			try {
+				bp.commands.startDevServer({ directory, port: 3000 });
+			} catch (err) {
+				vscode.window.showErrorMessage(err);
+			}
+		} else {
+			vscode.window.showErrorMessage('Working folder not found, open a folder an try again');
+		}
 	});
 
 	context.subscriptions.push(disposable);
